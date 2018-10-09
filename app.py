04742e636dd25aa302f056f220febe85ca738a7f
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-09 16:20:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import url_for
 from firebase_admin import credentials
 from firebase_admin import db as dbFirebase
 import firebase_admin
@@ -20,34 +23,37 @@ from models import *
 
 @app.route("/")
 def inicio():
-	if session.get('AUTH') != None:
-		if session['AUTH'] == True:
-			redirect('/index')
+	if session['AUTH'] == True:
+		return index()
 	else:
 		session['AUTH'] = False
-	return render_template('login.html', val = session['AUTH'])
+		return render_template('login.html', val = session['AUTH'])
 
 @app.route('/profesor/<int:id>')
 def profesor(id):
-	if session.get('AUTH') == True:
-		profesor = Profesor.query.filter_by(idProfesor=id).first()
-		alumnoid = session['id']
-		citas = Cita.query.filter_by(idAlumno=alumnoid)
-		return render_template('detalleProfesor.html', profesor=profesor, citas=citas)
-	else:
-		redirect('/index')
+	profesor = Profesor.query.filter_by(idProfesor=id).first()
+	alumnoid = session['id']
+	citas = Cita.query.filter_by(idAlumno=alumnoid)
+	return render_template('detalleProfesor.html', profesor=profesor, citas=citas)
+
+@app.route('/misCitas')
+def citas():
+	alumnoid = session['id']
+	citas = Cita.query.filter_by(idAlumno=alumnoid)
+	return render_template('misCitas.html', citas=citas)
 
 @app.route('/login', methods=['POST'])
 def login():
-	alumno = Alumno.query.filter_by(usuarioAlumno=request.form['uname'], contrasena=request.form['psw']).first()
-	if alumno:
-		session['AUTH'] = True
-		session['id'] = alumno.idAlumno
-		session['username'] = alumno.usuarioAlumno
-		session['nombre'] = alumno.nombre
-		return index()
-	else:
-		return render_template('login.html', val = True)
+	if request.method == 'POST':
+		alumno = Alumno.query.filter_by(usuarioAlumno=request.form['uname'], contrasena=request.form['psw']).first()
+		if alumno:
+			session['AUTH'] = True
+			session['id'] = alumno.idAlumno
+			session['username'] = alumno.usuarioAlumno
+			session['nombre'] = alumno.nombre
+			return index()
+		else:
+			return render_template('login.html', val = True)
 
 def do_the_login():
 	connectToFirebase()
@@ -66,47 +72,34 @@ def do_the_login():
 			break
 	if Test.AUTH == True:
 		session['username'] = request.form['uname']
-		redirect(url_for('index'))
+		return index()
 	else:
 		return render_template('login.html', val = True)
 
 @app.route("/index")
 def index():
-	if session.get('AUTH') == True:
+	if (session['AUTH'] == True):
 		profesores = Profesor.query.all()
 		alumnoid = session['id']
 		citas = Cita.query.filter_by(idAlumno=alumnoid)
 		return render_template('index.html', profesores=profesores, citas=citas)
 	else:
-		return inicio()
+		return render_template('login.html', val = session['AUTH'])
 
 @app.route("/reservarCita/<int:idAs>")
 def reservarCita(idAs):
-	if session.get('AUTH') == True:
-		session['idAs'] = idAs
-		asesoria = Asesoria.query.filter_by(idAsesoria=idAs).first()
-		return render_template('reservarCita.html', asesoria=asesoria)
-	else:
-		return inicio()
+	session['idAs'] = idAs
+	asesoria = Asesoria.query.filter_by(idAsesoria=idAs).first()
+	return render_template('reservarCita.html', asesoria=asesoria)
 
 @app.route("/generarReserva", methods=['POST'])
 def generarReserva():
-	if session.get('AUTH') == True:
-		fecha = datetime.now().date()
-		cita = Cita(idAlumno=session['id'], idAsesoria=session['idAs'], fecha=fecha, pregunta=request.form['consulta'])
-		db.session.add(cita)
-		db.session.commit()
-		asesoria = Asesoria.query.filter_by(idAsesoria=session['idAs']).first()
-		return render_template('reservarCita.html', asesoria=asesoria)
-	else:
-		return inicio()
-	
-@app.route("/cancelarReserva/<int:id>")
-def cancelarReserva(id):
-	cita = Cita.query.filter_by(idCita = id).first()
-	db.session.delete(cita)
+	fecha = datetime.now().date()
+	cita = Cita(idAlumno=session['id'], idAsesoria=session['idAs'], fecha=fecha, pregunta=request.form['consulta'])
+	db.session.add(cita)
 	db.session.commit()
-	return redirect('/profesor/1#miscitas')
+	asesoria = Asesoria.query.filter_by(idAsesoria=session['idAs']).first()
+	return render_template('reservarCita.html', asesoria=asesoria)
 
 def connectToFirebase():
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -115,8 +108,12 @@ def connectToFirebase():
 		cred = credentials.Certificate(json_URL)
 		firebase_admin.initialize_app(cred, {'databaseURL' : 'https://crashsoft-e0a3e.firebaseio.com/'})
 
-#def connectToPostgres():
-	#
+@app.route("/cancelarReserva", methods=['POST'])
+def cancelarReserva():
+	cita = Cita.query.filter_by(idCita = request.form['idCita'])
+	db.session.delete(cita)
+	db.session.commit()
+	return redirect('/index')
 
 def init():
     port = int(os.environ.get('PORT', 5000))
