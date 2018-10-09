9fc6f5ae0f2271f1175a2d6a9f0820d8a5dd3f80
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-09 15:12:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import url_for
+from flask import Flask, request, render_template, url_for, redirect
 from firebase_admin import credentials
 from firebase_admin import db as dbFirebase
 import firebase_admin
