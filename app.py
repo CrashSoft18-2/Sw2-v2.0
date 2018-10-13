402c5d14f22bc9813b6fa5afc854f1216d70ac2b
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-13 17:17:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 import config
 import views
 
+def init():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    #firebase_admin.initialize_app()
+
+if __name__ == "__main__":
+	init()
