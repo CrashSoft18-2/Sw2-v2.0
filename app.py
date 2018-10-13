644eb4d9f873768dfef49e19d87669430d5d0efb
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-13 15:51:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import config
 import views
 import os
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
 def init():
     port = int(os.environ.get('PORT', 5000))
