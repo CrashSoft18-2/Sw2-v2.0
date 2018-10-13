6febe965a9039c4aacfed1a600195051572649f1
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-13 15:15:30 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
