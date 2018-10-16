fef1dee1d29b8a887b4682df4dc033e5b8a5da4c
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-15 19:32:53 -0500

    Update jquery.ui.chatbox.js

--- static/js/jquery.ui.chatbox.js
+++ static/js/jquery.ui.chatbox.js
@@ -135,7 +135,7 @@
                        function() { uiChatboxTitlebarClose.removeClass('ui-state-hover'); })
                 .click(function(event) {
                     //uiChatbox.hide();
-                    console.log(uiChatbox.next());
+                    console.log(uiChatbox.next().style.right);
                     uiChatbox.remove();
                     self.options.boxClosed(self.options.id);
                     return false;
