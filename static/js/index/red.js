57b13ad6739347ded1d39d59ffce10b56be7740e
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-22 17:10:28 -0500

    Update red.js

--- static/js/index/red.js
+++ static/js/index/red.js
@@ -2,7 +2,7 @@ function displayAlumno(){
   window.location.replace("/alumno")
 }
 function displayProfesor(){
-  //window.location.replace("/profesor")
+  window.location.replace("/profesor")
 }
 function displayAdministrador(){
   //window.location.replace("/administrador")
