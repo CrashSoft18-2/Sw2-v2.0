6f71bb017c56762587efe89ec2be6a9355011b00
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-22 15:00:56 -0500

    Create red.js

---
+++ static/js/red.js
@@ -0,0 +1,9 @@
+function displayAlumno(){
+  window.location.replace("/alumno")
+}
+function displayProfesor(){
+  //window.location.replace("/profesor")
+}
+function displayAdministrador(){
+  //window.location.replace("/administrador")
+}
