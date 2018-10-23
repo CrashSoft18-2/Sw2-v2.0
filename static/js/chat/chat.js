9c2cf16a067b8addd9c94066e3b03b5d29030567
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-23 18:33:59 -0500

    Update chat.js

--- static/js/chat/chat.js
+++ static/js/chat/chat.js
@@ -70,7 +70,7 @@ var mensajes = []
 					} else if(snapshot.val() == "2"){
 						user = buscarUsuario(childsnapshot.val().from);
 					}
-					onChildAdded(user);
+					onChildAdded(user.user);
 				});
         });
     }
