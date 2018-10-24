a974860a1cc7db985972cc8a9fb59881c06f53b9
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-10-24 07:44:44 -0500

    Update views.py

--- profesor/views.py
+++ profesor/views.py
@@ -5,6 +5,9 @@ from flask import session, request, render_template, url_for, redirect
 import datetime
 
 @app.route("/profesor")
+def profesorLogin():
+	return render_template('profesor/login.html')
+
+@app.route("/profesor")
 def profesorUC():
 	return render_template('temp/temp.html')
-
