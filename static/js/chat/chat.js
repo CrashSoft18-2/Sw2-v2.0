91064e7eed5d72fedf09704ba65018fce7bef5cd
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-10-24 17:07:16 -0500

    Update chat.js

--- static/js/chat/chat.js
+++ static/js/chat/chat.js
@@ -27,9 +27,9 @@ function filter(){
 	
 	$('.user_item').each(function(){
 		if(this.textContent.indexOf(expr) == -1){
-			this.hide();
+			this.css("display", "none");
 		} else {
-			this.show();
+			this.css("display", "block");
 		}
 	});
 }
