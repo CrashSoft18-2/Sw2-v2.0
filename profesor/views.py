70d956765af0035b1199e3054e861870e0a9ec76
CrashSoft18-2 <43153625+CrashSoft18-2@users.noreply.github.com>
2018-10-23 20:52:20 -0500

    Update views.py

--- profesor/views.py
+++ profesor/views.py
@@ -1,3 +1,8 @@
+from utils import *
+from models import *
+from flask import session, request, render_template, url_for, redirect
+import datetime
+
 @app.route("/profesor")
 def inicio():
 	return render_template('profesor/login.html', val=False)
