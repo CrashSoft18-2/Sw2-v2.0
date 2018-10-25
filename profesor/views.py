77f1c30ced314565a59fa52c25d24b4ded804bfc
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-10-25 16:34:25 -0500

    Update views.py

--- profesor/views.py
+++ profesor/views.py
@@ -20,7 +20,38 @@ def profesorInicio():
 	else:
 		session['AUTH'] = 'Vacio'
 		return render_template('profesor/login.html', val = False)
+	
+
 
 @app.route("/profesor/login", methods=['POST'])
 def loginProfesor():
-	return render_template('temp/temp.html')
+	pw = encode(request.form['uname'], request.form['psw'])
+	profesor = Profesor.query.filter_by(usuarioProfesor=request.form['uname'], contrasena=pw).first()
+	if alumno:
+		session['AUTH'] = 'Profesor'
+		session['id'] = profesor.idProfesor
+		session['username'] = profesor.usuarioProfesor
+		session['nombre'] = profesor.nombre
+		return redirect("/profesor/displayProximasAsesorias")
+	else:
+		return render_template('profesor/login.html', val = True)
+
+	
+@app.route("profesor/displayProximasAsesorias")
+def index():
+	if session.get('AUTH') != None:
+		if session['AUTH'] == 'Profesor':
+			profesor = Profesor.query.filter_by(usuarioProfesor=session['username']).first()
+			username = session['username']
+			return render_template('profesor/proximasAsesorias.html', profesor = profesor, usuario = username)
+		elif session['AUTH'] == 'Alumno':
+			return redirect('/alumno')
+		elif session['AUTH'] == 'Administrador':
+			return redirect('/administrador')
+		else:
+			session['AUTH'] = 'Vacio'
+			return redirect("/profesor")
+	else:
+		session['AUTH'] = 'Vacio'
+		return redirect("/profesor")
+	
