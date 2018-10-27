bd5fadfcd6518c149618e5aa59495ad23071f74b
AshMGM <38756768+AshMGM@users.noreply.github.com>
2018-10-27 15:12:18 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from app import app
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/test'#os.environ.get('DATABASE_URL')
 app.secret_key = b'1234'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
