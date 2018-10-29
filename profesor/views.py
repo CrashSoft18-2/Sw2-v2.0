5b084369dcfc4a14203a3b2e08455fcec71060e0
AshMGM <38756768+AshMGM@users.noreply.github.com>
2018-10-29 12:50:56 -0500

    Update views.py

--- profesor/views.py
+++ profesor/views.py
@@ -1,6 +1,6 @@
 from utils import *
-from profesor.models import *
 from alumno.models import *
+from profesor.models import *
 from app import app
 from flask import session, request, render_template, url_for, redirect
 import datetime
