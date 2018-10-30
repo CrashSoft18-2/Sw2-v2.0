b1ce82c438fda0717c2314fea259bc94445df347
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-10-30 12:10:28 -0500

    Update models.py

--- alumno/models.py
+++ alumno/models.py
@@ -5,7 +5,7 @@ class Alumno(db.Model):
     usuarioAlumno = db.Column(db.String, unique=True, nullable=False)
     contrasena = db.Column(db.String, nullable=False)
     nombre = db.Column(db.String, nullable=False)
-    #carrera = db.Column(db.String, nullable=False)
+    carrera = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return '<Alumno %r>' % self.usuarioAlumno
