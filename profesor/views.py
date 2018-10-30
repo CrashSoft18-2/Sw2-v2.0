285e2d2c782ad6ab4ce3a43b6c94299c10ce7bae
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-10-30 14:40:22 -0500

    Update views.py

--- profesor/views.py
+++ profesor/views.py
@@ -1,7 +1,6 @@
 from utils import *
-#from alumno.models import *
+from alumno.models import *
 from profesor.models import *
-from alumno.models import Cita
 from app import app
 from flask import session, request, render_template, url_for, redirect
 import datetime
