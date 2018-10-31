f45df2dbf52d5fa14b4b16c391eb48480a80c43a
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-10-30 20:24:00 -0500

    Update models.py

--- profesor/models.py
+++ profesor/models.py
@@ -64,3 +64,23 @@ class Seminario(db.Model):
 
     def __repr__(self):
         return '<Seminario %r>' % self.idSeminario
+    
+ class Registro(db.Model):
+    idRegistro = db.Column(db.Integer, primary_key=True)
+    idAsesoria = db.Column(db.Integer, db.ForeignKey('asesoria.idAsesoria'), nullable=False)
+    asesoria = db.relationship('Asesoria', backref=db.backref('registros', lazy=True))
+    temaTratado = db.Column(db.String, nullable=False)
+    conclusion = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return '<Registro %r>' % self.idRegistro
+    
+class registroSeminario(db.Model):
+    idRegistroSeminario = db.Column(db.Integer, primary_key=True)
+    idSeminario = db.Column(db.Integer, db.ForeignKey('seminario.idSeminario'), nullable=False)
+    seminario = db.relationship('Seminario', backref=db.backref('seminarios', lazy=True))
+    idAlumno = db.Column(db.Integer, db.ForeignKey('alumno.idAlumno'), nullable=False)
+    alumno = db.relationship('Seminario', backref=db.backref('alumnos', lazy=True))
+
+    def __repr__(self):
+        return '<RegistroSeminario %r>' % self.idRegistroSeminario
