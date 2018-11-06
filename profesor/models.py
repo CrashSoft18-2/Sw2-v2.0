65cb157cfe6a794f8346d980fa99d591290318e9
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-11-05 21:27:12 -0500

    Update models.py

--- profesor/models.py
+++ profesor/models.py
@@ -7,7 +7,7 @@ class Profesor(db.Model):
     nombre = db.Column(db.String, nullable=False)
     carrera = db.Column(db.String, nullable=False)
     foto = db.Column(db.String, nullable=False)
-    asesorias = db.relationship('Asesoria', order_by='Asesoria.fecha.asc()')
+    asesorias = db.relationship('Asesoria', order_by='Asesoria.fecha.asc()', cascade="all,delete")
 
     def __repr__(self):
         return '<Profesor %r>' % self.usuarioProfesor
@@ -22,9 +22,9 @@ class Curso(db.Model):
 class Seccion(db.Model):
     idSeccion = db.Column(db.Integer, primary_key=True)
     idCurso = db.Column(db.Integer, db.ForeignKey('curso.idCurso'), nullable=False)
-    curso = db.relationship('Curso', backref=db.backref('secciones', lazy=True))
+    curso = db.relationship('Curso', backref=db.backref('secciones', lazy=True, cascade="all,delete"))
     idProfesor = db.Column(db.Integer, db.ForeignKey('profesor.idProfesor'), nullable=False)
-    profesor = db.relationship('Profesor', backref=db.backref('secciones', lazy=True))
+    profesor = db.relationship('Profesor', backref=db.backref('secciones', lazy=True, cascade="all,delete"))
     numeroSeccion = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
@@ -45,9 +45,9 @@ class Asesoria(db.Model):
 class Cita(db.Model):
     idCita = db.Column(db.Integer, primary_key=True)
     idAlumno = db.Column(db.Integer, db.ForeignKey('alumno.idAlumno'), nullable=False)
-    alumno = db.relationship('Alumno', backref=db.backref('citas', lazy=True))
+    alumno = db.relationship('Alumno', backref=db.backref('citas', lazy=True, cascade="all,delete"))
     idAsesoria = db.Column(db.Integer, db.ForeignKey('asesoria.idAsesoria'), nullable=False)
-    asesoria = db.relationship('Asesoria', backref=db.backref('citas', lazy=True))
+    asesoria = db.relationship('Asesoria', backref=db.backref('citas', lazy=True, cascade="all,delete"))
     fecha = db.Column(db.Date, nullable=False) #fecha??
     pregunta = db.Column(db.String, nullable=False)
 
@@ -57,7 +57,7 @@ class Cita(db.Model):
 class Seminario(db.Model):
     idSeminario = db.Column(db.Integer, primary_key=True)
     idProfesor = db.Column(db.Integer, db.ForeignKey('profesor.idProfesor'), nullable=False)
-    profesor = db.relationship('Profesor', backref=db.backref('seminarios', lazy=True))
+    profesor = db.relationship('Profesor', backref=db.backref('seminarios', lazy=True, cascade="all,delete"))
     topic = db.Column(db.String, nullable=False)
     fecha = db.Column(db.Date, nullable=False)
     hora = db.Column(db.String, nullable=False)
@@ -69,7 +69,7 @@ class Seminario(db.Model):
 class Registro(db.Model):
     idRegistro = db.Column(db.Integer, primary_key=True)
     idAsesoria = db.Column(db.Integer, db.ForeignKey('asesoria.idAsesoria'), nullable=False)
-    asesoria = db.relationship('Asesoria', backref=db.backref('registros', lazy=True))
+    asesoria = db.relationship('Asesoria', backref=db.backref('registros', lazy=True, cascade="all,delete"))
     temaTratado = db.Column(db.String, nullable=False)
     conclusion = db.Column(db.String, nullable=False)
 
@@ -79,7 +79,7 @@ class Registro(db.Model):
 class registroSeminario(db.Model):
     idRegistroSeminario = db.Column(db.Integer, primary_key=True)
     idSeminario = db.Column(db.Integer, db.ForeignKey('seminario.idSeminario'), nullable=False)
-    seminario = db.relationship('Seminario', backref=db.backref('rseminarios', lazy=True))
+    seminario = db.relationship('Seminario', backref=db.backref('rseminarios', lazy=True, cascade="all,delete"))
     idAlumno = db.Column(db.Integer, db.ForeignKey('alumno.idAlumno'), nullable=False)
     alumno = db.relationship('Alumno', backref=db.backref('alumnos', lazy=True, cascade="all,delete"))
 
