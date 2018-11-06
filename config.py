05b9d6a747ee3c0ffc75edc66f89b952d83abd4b
dianaayalag <33363220+dianaayalag@users.noreply.github.com>
2018-11-06 10:50:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from app import app
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://inrbkbfqwjvcnb:83df34940918d53940c4bd30b5a185d3d79726cd36230f4a402f4a8f8579e680@ec2-174-129-35-61.compute-1.amazonaws.com:5432/da8gvk43j45moa'#os.environ.get('DATABASE_URL')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://inrbkbfqwjvcnb:83df34940918d53940c4bd30b5a185d3d79726cd36230f4a402f4a8f8579e680@ec2-174-129-35-61.compute-1.amazonaws.com:5432/da8gvk43j45moa'#os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.secret_key = b'1234'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
